56e363410db7f10005a9c67d4b3de53c5b3553ef
Bayel-Tashpulatov <bayeltashpulatov1@gmail.com>
2025-04-17 23:02:01 +0600

    update ai chat

--- lib/features/chat/ai_chat_service.dart
+++ lib/features/chat/ai_chat_service.dart
@@ -1,8 +1,9 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-const openAiApiKey = '';
-const openAiEndpoint = 'https://api.openai.com/v1/chat/completions';
+const openAiApiKey =
+    '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';
+const openAiEndpoint = 'https://openrouter.ai/api/v1/chat/completions';
 
 Future<String> sendMessageToAI(String message) async {
   final response = await http.post(
@@ -10,9 +11,10 @@ Future<String> sendMessageToAI(String message) async {
     headers: {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer $openAiApiKey',
+      'X-Title': 'Memly',
     },
     body: jsonEncode({
-      'model': 'gpt-3.5-turbo',
+      'model': 'openai/gpt-3.5-turbo',
       'messages': [
         {'role': 'user', 'content': message},
       ],
@@ -23,7 +25,7 @@ Future<String> sendMessageToAI(String message) async {
     final data = jsonDecode(response.body);
     final reply = data['choices'][0]['message']['content'];
     return reply.trim();
-  }else{
+  } else {
     print('Ошибка:  ${response.body}');
     return 'Ошибка. Попробуйте еще раз.';
   }

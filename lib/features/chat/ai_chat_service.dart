30d47b8b95f6895e9382a3593b8e3e022b3cbcfb
Bayel-Tashpulatov <bayeltashpulatov1@gmail.com>
2025-05-05 14:06:08 +0600

    updated

--- lib/features/chat/ai_chat_service.dart
+++ lib/features/chat/ai_chat_service.dart
@@ -1,32 +1,38 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-const openAiApiKey =
-    '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';
+const openAiApiKey = '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';
 const openAiEndpoint = 'https://openrouter.ai/api/v1/chat/completions';
 
 Future<String> sendMessageToAI(String message) async {
-  final response = await http.post(
-    Uri.parse(openAiEndpoint),
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer $openAiApiKey',
-      'X-Title': 'Memly',
-    },
-    body: jsonEncode({
-      'model': 'openai/gpt-3.5-turbo',
-      'messages': [
-        {'role': 'user', 'content': message},
-      ],
-    }),
-  );
+  try {
+    final response = await http.post(
+      Uri.parse(openAiEndpoint),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $openAiApiKey',
+        'X-Title': 'Memly',
+      },
+      body: jsonEncode({
+        'model': 'google/gemma-2-9b-it:free',
+        'messages': [
+          {'role': 'user', 'content': message},
+        ],
+      }),
+    );
 
-  if (response.statusCode == 200) {
-    final data = jsonDecode(response.body);
-    final reply = data['choices'][0]['message']['content'];
-    return reply.trim();
-  } else {
-    print('Ошибка:  ${response.body}');
+    if (response.statusCode == 200) {
+      final data = jsonDecode(utf8.decode(response.bodyBytes));
+      final reply = data['choices']?.isEmpty ?? true
+          ? 'Ответ не получен.'
+          : data['choices'][0]['message']['content'];
+      return reply.trim();
+    } else {
+      print('Ошибка: ${response.statusCode} ${utf8.decode(response.bodyBytes)}');
+      return 'Ошибка. Попробуйте еще раз.';
+    }
+  } catch (e) {
+    print('Произошла ошибка: $e');
     return 'Ошибка. Попробуйте еще раз.';
   }
 }
